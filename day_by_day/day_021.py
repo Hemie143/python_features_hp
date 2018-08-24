8836cc6fcab4ef288114ea9ac627da24bcae74e9
Laurent Hemeryck <hemie143@gmail.com>
2018-08-24 12:52:11 +0200

    Day 21

---
+++ day_by_day/day_021.py
@@ -0,0 +1,6 @@
+# Two ways to run the code:
+# 1. run directly   : __name__ = __main__
+# 2. import module  : __name__ = magical_universe
+
+# https://stackoverflow.com/questions/419163/what-does-if-name-main-do
+# https://docs.python.org/3/library/__main__.html
